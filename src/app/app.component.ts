0ccd4afa4a60e62af5329a5095117428110d8e66
ADRIANO23F <diegopopi1239@gmail.com>
2024-11-23 01:10:08 -0500

    Diseño de regsitro, Login mejorado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FirebaseTSApp} from 'firebasets/firebasetsApp/firebaseTSApp';
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 import { environment } from '../environments/environment';
 
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatCardModule } from '@angular/material/card';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule],
+  imports: [RouterOutlet, MatCardModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RedSocial';
+ 
 
-  constructor(){
-FirebaseTSApp.init(environment.firebaseConfig )
+  constructor() {
+    FirebaseTSApp.init(environment.firebaseConfig)
 
   }
+ 
+  
 
 }
+
--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -1,20 +1,40 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-authenticator',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './authenticator.component.html',
   styleUrl: './authenticator.component.css'
 })
 export class AuthenticatorComponent {
-  onLoginClick(){
-
+  state = AuthenticatorCompState.LOGIN;
+  onForgotPasswordClick() {
+    this.state = AuthenticatorCompState.FORGOT_PASSWORD;
   }
-  onCreateAccountClick(){
 
+  onCreateAccountClick() {
+    this.state = AuthenticatorCompState.REGISTER;
   }
-  
-  onForgotPaswordClick(){
 
+  onLoginClick() {
+    this.state = AuthenticatorCompState.LOGIN;
   }
+
+  isLoginState() {
+    return this.state == AuthenticatorCompState.LOGIN;
+  }
+
+  isRegisterState() {
+    return this.state == AuthenticatorCompState.REGISTER;
+  }
+  isForgotPasswordState() {
+    return this.state == AuthenticatorCompState.FORGOT_PASSWORD;
+  }
+}
+
+export enum AuthenticatorCompState {
+  LOGIN,
+  REGISTER,
+  FORGOT_PASSWORD
 }
