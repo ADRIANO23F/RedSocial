2c3ebaf6ccbdb697785fc1a79d7f4c0492c839cb
ADRIANO23F <diegopopi1239@gmail.com>
2024-11-23 00:13:16 -0500

    FirebaseTS instalado

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+
+
+export const environment = {
+    production : false,
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+firebaseConfig : {
+    apiKey: "AIzaSyB3Ym3mkfcLfsAWWv0TpX5r9S19XW6WGM8",
+    authDomain: "paginaweb-dd7a4.firebaseapp.com",
+    projectId: "paginaweb-dd7a4",
+    storageBucket: "paginaweb-dd7a4.firebasestorage.app",
+    messagingSenderId: "543054598018",
+    appId: "1:543054598018:web:55995f104a70864909499b",
+    measurementId: "G-LQ5ETS273S"
+  }
+
+
+}
