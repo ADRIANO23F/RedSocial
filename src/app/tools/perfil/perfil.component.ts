173f81cf6c24c425c6722c6913b4104df7136d01
ADRIANO23F <diegopopi1239@gmail.com>
2024-11-26 23:54:45 -0500

    Agregando Funcion de Cargar datos en Firebase

--- src/app/tools/perfil/perfil.component.ts
+++ src/app/tools/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
 
 @Component({
   selector: 'app-perfil',
@@ -10,7 +12,45 @@ import { CommonModule } from '@angular/common';
 })
 export class PerfilComponent {
   @Input() show: boolean = true; // Inicializar directamente
+
+  firestore: FirebaseTSFirestore;
+  auth: FirebaseTSAuth;
+
+  constructor(){
+    this.firestore = new FirebaseTSFirestore();
+    this.auth = new FirebaseTSAuth();
+  }
+
   cerrarSesion() {
     this.show = false; // Esto cerrará el perfil
+  }
+  onContinueCLICK(
+    nameInput:HTMLInputElement,
+    descriptionInput:HTMLTextAreaElement
+
+  ){
+    let name = nameInput.value;
+    let description = descriptionInput.value;
+    this.firestore.create(
+      {
+        path: ["Users", this.auth.getAuth().currentUser?.uid ?? 'default-uid'],
+
+        data: {
+          publicName: name,
+          description: description
+
+        },
+        onComplete: (docId) =>{
+          alert("Perfil Creado");
+          nameInput.value= "";
+          descriptionInput.value="";
+
+        },
+        onFail: (err) => {
+
+        }
+      }
+    );
+
   }
 }
