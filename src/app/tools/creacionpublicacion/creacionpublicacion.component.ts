482085e59da8a644f00772a8b0cda482e1857dc9
ADRIANO23F <diegopopi1239@gmail.com>
2024-11-29 00:10:46 -0500

    Mejorando el cuadro de dialogo

--- src/app/tools/creacionpublicacion/creacionpublicacion.component.ts
+++ src/app/tools/creacionpublicacion/creacionpublicacion.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-creacionpublicacion',
-  imports: [],
+  standalone: true, // Asegúrate de que el componente es standalone
   templateUrl: './creacionpublicacion.component.html',
-  styleUrl: './creacionpublicacion.component.css'
+  styleUrls: ['./creacionpublicacion.component.css'],
+  imports: [MatCardModule, MatIconModule, MatButtonModule] // Importa los módulos necesarios
 })
-export class CreacionpublicacionComponent {
-
-}
+export class CreacionpublicacionComponent {}
