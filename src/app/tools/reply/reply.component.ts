e41eb77aaf9fef36a746b55c7224ac0cd4dbb7de
ADRIANO23F <diegopopi1239@gmail.com>
2024-12-06 00:19:38 -0500

    Agregando la Funcion para ver los comentarios de cada publicacion

--- src/app/tools/reply/reply.component.ts
+++ src/app/tools/reply/reply.component.ts
@@ -1,39 +1,66 @@
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
+import { FirebaseTSFirestore, OrderBy } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { timestamp } from 'rxjs';
 import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
+import { CommonModule } from '@angular/common';  // Importar CommonModule
 import { AppComponent } from '../../app.component';
+
 @Component({
   selector: 'app-reply',
-  standalone: true, // Asegúrate de que está definido como standalone
-  imports: [MatButtonModule, MatIconModule],
+  standalone: true,
+  imports: [MatButtonModule, MatIconModule, CommonModule],  // Asegúrate de incluir CommonModule
   templateUrl: './reply.component.html',
   styleUrls: ['./reply.component.css']
 })
 export class ReplyComponent {
   firestore = new FirebaseTSFirestore();
-  constructor(@Inject(MAT_DIALOG_DATA) private postId: string){}
-onSendClick(commentInput: HTMLInputElement){
-  if(!(commentInput.value.length > 0)) return;
-  this.firestore.create(
-    {
-      path: ["Posts", this.postId, "PostComments"],
+  comments: comment[] = [];
+
+  constructor(@Inject(MAT_DIALOG_DATA) private postId: string) {}
+
+  ngOnInit(): void {
+    this.getComments();
+  }
+
+  getComments() {
+    this.firestore.listenToCollection(
+      {
+        name: 'Post Comments',
+        path: ['Posts', this.postId, 'PostComments'],
+        where: [new OrderBy('timestamp', 'asc')],
+        onUpdate: (result) => {
+          result.docChanges().forEach((postCommentDoc) => {
+            if (postCommentDoc.type === 'added') {
+              this.comments.unshift(<comment>postCommentDoc.doc.data());
+            }
+          });
+        },
+      }
+    );
+  }
+
+  onSendClick(commentInput: HTMLInputElement) {
+    if (!(commentInput.value.length > 0)) return;
+    this.firestore.create({
+      path: ['Posts', this.postId, 'PostComments'],
       data: {
         comment: commentInput.value,
         creatorId: AppComponent.getUserDocument()?.userId,
         creatorName: AppComponent.getUserDocument()?.publicName,
-        timestamp: FirebaseTSApp.getFirestoreTimestamp()
+        timestamp: FirebaseTSApp.getFirestoreTimestamp(),
       },
       onComplete: (docId) => {
-        commentInput.value= "";
-      }
-
-    
-    }
-  );
-
+        commentInput.value = '';
+      },
+    });
+  }
 }
+
+export interface comment {
+  creatorId: string;
+  creatorName: string;
+  comment: string;
+  timestamp: firebase.default.firestore.Timestamp;
 }
